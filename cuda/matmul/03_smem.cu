55a39bc6a62d969ee1ec3e1b672f17bd74cd168b
JINO-ROHIT <find.jinorohit@gmail.com>
2026-04-26 15:53:00 +0530

    ncu rep for smem + explanation

--- cuda/matmul/03_smem.cu
+++ cuda/matmul/03_smem.cu
@@ -21,8 +21,12 @@ __global__ void smem_kernel(int M, int N, int K,
     float tmp = 0.0f;
     // we need to move across the k dimension loading tiles of size BLOCKSIZE into the shared mem
     for(int tile_idx = 0; tile_idx < K / BLOCKSIZE; tile_idx += 1){
-        // find the correct row of A, then the correct tile of A, and then move within the tile
-        As[thread_row][thread_col] = A[global_row * K + tile_idx * BLOCKSIZE + thread_col];
+        // to understand this a bit better, think this way
+        // we have A = M x K and B = K x N
+        // A[row, K] = [row * K + k]
+        // B[K, col] = [K * N + col]
+        // same pattern for loading
+        As[thread_row][thread_col] = A[global_row * K + (tile_idx * BLOCKSIZE + thread_col)];
         Bs[thread_row][thread_col] = B[(tile_idx * BLOCKSIZE + thread_row) * N + global_col];
 
         __syncthreads();
@@ -68,23 +72,23 @@ int main() {
     dim3 blockDim(BLOCKSIZE, BLOCKSIZE);  // 16x16 = 256 threads
     
     smem_kernel<BLOCKSIZE><<<gridDim, blockDim>>>(M, N, K, d_A, d_B, d_C);    
-    cudaDeviceSynchronize();
+    // cudaDeviceSynchronize();
 
-    cudaEvent_t tStart, tStop;
-    cudaEventCreate(&tStart);
-    cudaEventCreate(&tStop);
+    // cudaEvent_t tStart, tStop;
+    // cudaEventCreate(&tStart);
+    // cudaEventCreate(&tStop);
 
-    cudaEventRecord(tStart);
-    smem_kernel<BLOCKSIZE><<<gridDim, blockDim>>>(M, N, K, d_A, d_B, d_C);    
-    cudaEventRecord(tStop);
-    cudaEventSynchronize(tStop);
+    // cudaEventRecord(tStart);
+    // smem_kernel<BLOCKSIZE><<<gridDim, blockDim>>>(M, N, K, d_A, d_B, d_C);    
+    // cudaEventRecord(tStop);
+    // cudaEventSynchronize(tStop);
 
-    float ms = 0.0f;
-    cudaEventElapsedTime(&ms, tStart, tStop);
+    // float ms = 0.0f;
+    // cudaEventElapsedTime(&ms, tStart, tStop);
 
-    double flops  = 2.0 * M * N * K;                
-    double tflops = flops / (ms * 1e9);     
-    printf("kernel time : %.4f ms\n", ms);
-    printf("throughput  : %.4f TFLOP/s\n\n", tflops);
+    // double flops  = 2.0 * M * N * K;                
+    // double tflops = flops / (ms * 1e9);     
+    // printf("kernel time : %.4f ms\n", ms);
+    // printf("throughput  : %.4f TFLOP/s\n\n", tflops);
 
 }
--- cuda/matmul/profilers/README.md
+++ cuda/matmul/profilers/README.md
@@ -26,8 +26,26 @@ also look at the roofline model, we are severly on the memory bound side, but ev
 
 ![roofline model](assets/roofline_naive.png)
 
-2. `02_gemm_coalesce.ncu-rep` - nvcc -O3 -arch=sm_89 --generate-line-info -Xptxas -v 01_naive.cu -o test
+2. `02_gemm_coalesce.ncu-rep`
 
 ```
 this is actually pretty similar to the naive version and offers no perf boost compared because our memory access is already coalesced in v1.
+```
+
+3. `03_smem.ncu-rep`
+
+```
+1. fp32 compute throughput - 7% of peak
+
+2. compute throughput is 86% but still its the same Inst Executed Pipe Lsu that is the highest, we need the FMA to be more.
+
+3. warp stalls - now we have gotten rid of the lg throttle completely 
+- now currently the major stall is stall long scoreboard which is each warp of this workload spends 30.9 cycles being stalled waiting for a scoreboard dependency on a L1TEX (local, global, surface, texture) operation.
+- we also have barrier stall because of the new __sync__ we introduced.
+
+4. the L1 hit rate in the smem version has gone down a lot, since we unlike global memeory where we hit l1 cache each time, we moved it to shared memory.
+also the roofline is still pretty memory bound but a bit closer to the line so better.
+
+![roofline model](assets/roofline_smem.png)
+
 ```
